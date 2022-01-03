27ed1ae1ea94fbace450261856bb8a3ebd1991cf
Steffen Poulsen <26925392+Raunow@users.noreply.github.com>
2022-01-03 10:52:03 +0100

    Add files via upload

---
+++ Scripts/powershell/ExportTasks.ps1
@@ -0,0 +1,3 @@
+﻿ForEach ($task in Get-ScheduledTask -TaskPath "\Custom\*") {
+  Export-ScheduledTask -TaskPath $task.TaskPath -TaskName $task.TaskName | Out-File "H:\Tasks\$($task.TaskName).xml"
+};
