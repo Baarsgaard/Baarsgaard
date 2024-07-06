ca68f297e40e32dd14044808bf54c0a678d3016b
ste <steff.bpoulsen@gmail.com>
2024-07-06 19:52:19 +0200

    Update init.sh untested

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,39 +1,76 @@
 #!/usr/bin/bash
 
+#
+# USE ME
+# curl -s -L https://raw.githubusercontent.com/Baarsgaard/Baarsgaard/master/scripts/init.sh | bash
+#
+
 set -eu
 set -o pipefail
 
-#
-# USE ME
-# curl -s -L URL_TO_SCRIPT_HERE | bash
-#
+sudo apt update
+sudo apt upgrade -y
+sudo apt install zsh fzf wslu ripgrep shellcheck shfmt unzip gpg syncthing jq ansible \
+  docker-cd docker-ce-cli docker-compose-plugin containerd.io docker-buildx-plugin
 
-sudo pacman -Syu --noconfirm
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup component add rust-analyzer
 
-# Config Repo
+cargo install git-delta
+cargo install bat
+cargo install taplo-cli --features lsp
+cargo install lychee
+cargo install --git https://github.com/Feel-ix-343/markdown-oxide.git markdown-oxide
+cargo install --git https://github.com/astral-sh/ruff.git ruff
+cargo install typos-cli
+cargo install cargo-workspaces
+cargo install --locked cargo-autoinherit
+
 mkdir -p "$HOME/projects"
-git clone 'https://github.com/Raunow/Raunow.git' "$HOME/projects/Raunow"
+cd "$HOME/projects"
+git clone https://github.com/helix-editor/helix
+cd helix
+cargo install --locked --path helix-term
 
-overwrite() {
-  cp -f "$HOME/projects/Raunow/wsl/$1" "$HOME/$1"
-}
-overwrite ".zshrc"
-overwrite ".zprofile"
-overwrite ".vimrc"
-overwrite ".gitconfig"
+mkdir -p "$HOME/.config/helix"
+ln -Ts "$PWD/runtime" "$HOME/.config/helix/runtime"
+cd "$HOME"
 
-# ZSH
-sudo pacman -Sy zsh fzf zsh-syntax-highlighting
+mkdir -p "$HOME/.local/bin"
+curl -fsSL https://bun.sh/install | bash
+ln -Ts "$(which bun)" ~/.local/bin/node
+
+bun i -g \
+  dockerfile-language-server-nodejs \
+  vscode-langservers-extracted \
+  sql-language-server \
+  yaml-language-server@next \
+  @ansible/ansible-language-server \
+  bash-language-server
 
 # oh-my-zsh
 export CHSH='no'
 export RUNZSH='no'
 export KEEP_ZSHRC='yes'
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+export ZSH_CUSTOM='~/.oh-my-zsh/custom'
 mkdir -p "$ZSH_CUSTOM"
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 git clone https://github.com/unixorn/fzf-zsh-plugin.git "$ZSH_CUSTOM/plugins/fzf-zsh-plugin"
 git clone https://github.com/Aloxaf/fzf-tab "$ZSH_CUSTOM/plugins/fzf-tab"
 
 chsh -s $(which zsh)
+
+# Dotfiles
+git clone 'https://github.com/Baarsgaard/Baarsgaard.git' "$HOME/projects/Baarsgaard"
+
+# overwrite() {
+#   cp -f "$HOME/projects/Baarsgaard/wsl/$1" "$HOME/$1"
+# }
+# overwrite ".zshrc"
+# overwrite ".zprofile"
+# overwrite ".vimrc"
+# overwrite ".gitconfig"
