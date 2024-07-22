9ae9006a301c3ad6e41fa1d3efe14186bde284ec
Steffen Baarsgaard <steff.bpoulsen@gmail.com>
2024-07-22 19:37:07 +0200

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 set -euo pipefail
+echo -n Password: 
+read -s PW
 
 # Nix
-sh <(curl -L https://nixos.org/nix/install) --daemon
+curl -L https://nixos.org/nix/install | sh -s -- --daemon --yes
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
 nix --version
 
 # Home-Manager
+mkdir -p ~/.config/home-manager
+curl -fsSL https://raw.githubusercontent.com/Baarsgaard/Baarsgaard/master/dotfiles/home.nix > ~/.config/home-manager/home.nix
+
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
-
 nix-shell '<home-manager>' -A install
 
 home-manager build
 home-manager switch
 nix-collect-garbage
 
+echo $PW | sudo echo 'tmp'>/dev/null
 which zsh | sudo tee -a /etc/shells
-chsh -s "$(which zsh)"
+echo $PW | chsh -s "$(which zsh)"
